a06239031b6c6168bf07f2a0a596aed39097bb97
VHumFF <108907994+VHumFF@users.noreply.github.com>
2025-03-28 08:52:56 +0800

    -added logic to retrieve trending articles

---
+++ News_Platform/Controllers/ArticlesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using News_Platform.DTOs;
+using News_Platform.Services;
+
+namespace News_Platform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        private readonly IArticleService _articleService;
+
+        public ArticlesController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        [HttpGet("trending")]
+        public async Task<ActionResult<List<TrendingArticleDto>>> GetTrendingArticles([FromQuery] int limit = 20)
+        {
+            var trendingArticles = await _articleService.GetTrendingArticlesAsync(limit);
+            return Ok(trendingArticles);
+        }
+    }
+}
